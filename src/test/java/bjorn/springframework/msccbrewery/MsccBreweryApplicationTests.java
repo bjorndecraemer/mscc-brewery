2bbab5fd9d67181e550f49e229e129a442d1e54f
Björn De Craemer <bjorn.decraemer@yahoo.com>
2022-12-11 23:23:29 +0100

    Initial commit

---
+++ src/test/java/bjorn/springframework/msccbrewery/MsccBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package bjorn.springframework.msccbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsccBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
